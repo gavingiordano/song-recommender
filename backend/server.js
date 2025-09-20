88e9576b308c506fa3bafe8faddd180320af3680
Gavin Giordano <gavingiordano18@gmail.com>
2025-09-20 18:49:23 -0400

    switching to last.fm api from spotify api

--- backend/server.js
+++ backend/server.js
@@ -5,35 +5,8 @@ const port = 3000;
 const fetch = require('node-fetch');
 
 require('dotenv').config();
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-
-
-const getToken = async () => {
-    try {
-        const result = await fetch('https://accounts.spotify.com/api/token', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': 'Basic ' + Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64')
-            },
-            body: new URLSearchParams({
-                grant_type: 'client_credentials'
-            }),
-        });
-
-        const data = await result.json();
-        if (!data.access_token) {
-            throw new Error('No access token received from Spotify API');
-        }
-        return data.access_token
-
-        // !! tokens expire after one hour !!
-
-    } catch(error) {
-        throw new Error(`Error fetching access token from Spotify API: ${error.message}`);
-    }
-};
+const API_KEY = process.env.API_KEY;
+const SHARED_SECRET = process.env.SHARED_SECRET;
 
 
 app.get('/', (req, res) => {
