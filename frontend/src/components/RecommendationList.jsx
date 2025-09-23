020bd82f30c5a096f430e361f56ebfd28aa2193e
Gavin Giordano <gavingiordano18@gmail.com>
2025-09-23 01:46:27 -0400

    added the recommendations list to the frontend

--- frontend/src/components/RecommendationList.jsx
+++ frontend/src/components/RecommendationList.jsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function RecommendationList({ selectedSong }) {
+    const [recs, setRecs] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if (selectedSong) {
+            fetchRecommendations();
+        } else {
+            setRecs([]);
+            setLoading(false);
+        }
+    }, [selectedSong]);
+
+    const fetchRecommendations = async () => {
+        setLoading(true)
+        try {
+            const response = await fetch(`http://localhost:3000/recommendations?track=${selectedSong.name}&artist=${selectedSong.artist}`);
+            const data = await response.json();
+            setTimeout(() => {
+                setRecs(data);
+                setLoading(false);
+            }, 500);
+        } catch (error) {
+            console.error('Error fetching recommendations:', error);
+        }
+    };
+
     return (
-        <div>Test: Generate Button Clicked</div>
+        <div>
+            <h2>Songs similar to:</h2>
+            <p>{selectedSong.name} - {selectedSong.artist}</p>
+            {loading && (
+                <p>Loading recommendations...</p>
+            )}
+            {recs.length > 0 ? (
+                <ul>
+                    {recs.map((rec, index) => (
+                        <li key={index}>
+                            {rec.name} = {rec.artist}
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                !loading && (
+                    <p>No recommendations found</p>
+                )
+            )}
+        </div>
     );
 }
